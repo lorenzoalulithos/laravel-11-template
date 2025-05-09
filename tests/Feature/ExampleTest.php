113a015dabc0c7c6072d7c0e305cfd7a6b382faa
lorenzoalu <lorenzo.alu@lithos.com>
2025-05-09 12:29:47 +0200

    Improve namning

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -15,7 +15,7 @@ final class ExampleTest extends TestCase
      */
     public function the_application_returns_a_successful_response(): void
     {
-        $response = $this->get('/');
+        $response = $this->get('/up');
 
         $response->assertStatus(Response::HTTP_OK);
     }
